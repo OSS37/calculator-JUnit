a7eeb77cae1de1ed484c785df3c9893ad4aebf2e
OSS37 <suhanova375@gmail.com>
2023-05-20 23:07:36 +0300

    initial commit

---
+++ src/main/java/va/suhano/calculatorJUnit/CalculatorJUnitApplication.java
@@ -0,0 +1,13 @@
+package va.suhano.calculatorJUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorJUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/va/suhano/calculatorJUnit/CalculatorJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package va.suhano.calculatorJUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
